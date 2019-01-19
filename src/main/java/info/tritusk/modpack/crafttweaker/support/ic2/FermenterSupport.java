0ed7c6654f5749b67678acbf9efb9a28a69b4f17
3TUSK <urey.s.knowledge@gmail.com>
2019-01-19 00:13:26 -0800

    Migrate FermenterSupport to IAction; add HeatExchangerSupport

--- src/main/java/info/tritusk/modpack/crafttweaker/support/ic2/FermenterSupport.java
+++ src/main/java/info/tritusk/modpack/crafttweaker/support/ic2/FermenterSupport.java
@@ -1,9 +1,13 @@
 package info.tritusk.modpack.crafttweaker.support.ic2;
 
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.liquid.ILiquidStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
 import ic2.api.recipe.Recipes;
+import net.minecraftforge.fluids.FluidStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -14,7 +18,29 @@ public final class FermenterSupport {
 
     @ZenMethod
     public static void addRecipe(ILiquidStack output, ILiquidStack input, int heat) {
-        Recipes.fermenter.addRecipe(input.getName(), input.getAmount(), heat, output.getName(), output.getAmount());
+        CraftTweakerAPI.apply(new FermentationAction(CraftTweakerMC.getLiquidStack(input), CraftTweakerMC.getLiquidStack(output), heat));
+    }
+
+    private static final class FermentationAction implements IAction {
+
+        private final FluidStack input, output;
+        private final int heat;
+
+        FermentationAction(FluidStack input, FluidStack output, int heat) {
+            this.input = input;
+            this.output = output;
+            this.heat = heat;
+        }
+
+        @Override
+        public void apply() {
+            Recipes.fermenter.addRecipe(this.input.getFluid().getName(), input.amount, heat, output.getFluid().getName(), output.amount);
+        }
+
+        @Override
+        public String describe() {
+            return null;
+        }
     }
 
 }
---
+++ src/main/java/info/tritusk/modpack/crafttweaker/support/ic2/HeatExchangerSupport.java
@@ -0,0 +1,70 @@
+package info.tritusk.modpack.crafttweaker.support.ic2;
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.liquid.ILiquidDefinition;
+import crafttweaker.api.liquid.ILiquidStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import ic2.api.recipe.ILiquidHeatExchangerManager;
+import ic2.api.recipe.Recipes;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ModOnly("ic2")
+@ZenClass("mods.ic2.HeatExchanger")
+@ZenRegister
+public final class HeatExchangerSupport {
+
+    @ZenMethod
+    public static void addFluidCoolDown(ILiquidStack output, ILiquidStack input, int heat) {
+        CraftTweakerAPI.apply(new HeatExchangeAction(Recipes.liquidCooldownManager, CraftTweakerMC.getLiquidStack(input), CraftTweakerMC.getLiquidStack(input), heat));
+    }
+
+    @ZenMethod
+    public static void addFluidCoolDown(ILiquidDefinition output, ILiquidDefinition input, int heat) {
+        CraftTweakerAPI.apply(new HeatExchangeAction(Recipes.liquidCooldownManager, CraftTweakerMC.getFluid(input), CraftTweakerMC.getFluid(input), heat));
+    }
+
+    @ZenMethod
+    public static void addFluidHeatUp(ILiquidStack output, ILiquidStack input, int heat) {
+        CraftTweakerAPI.apply(new HeatExchangeAction(Recipes.liquidHeatupManager, CraftTweakerMC.getLiquidStack(input), CraftTweakerMC.getLiquidStack(input), heat));
+    }
+
+    @ZenMethod
+    public static void addFluidHeatUp(ILiquidDefinition output, ILiquidDefinition input, int heat) {
+        CraftTweakerAPI.apply(new HeatExchangeAction(Recipes.liquidHeatupManager, CraftTweakerMC.getFluid(input), CraftTweakerMC.getFluid(input), heat));
+    }
+
+    private static final class HeatExchangeAction implements IAction {
+
+        private final ILiquidHeatExchangerManager manager;
+        private final Fluid input, output;
+        private final int heat;
+
+        HeatExchangeAction(ILiquidHeatExchangerManager manager, FluidStack input, FluidStack output, int heat) {
+            this(manager, input.getFluid(), output.getFluid(), heat);
+        }
+
+        HeatExchangeAction(ILiquidHeatExchangerManager manager, Fluid input, Fluid output, int heat) {
+            this.manager = manager;
+            this.input = input;
+            this.output = output;
+            this.heat = heat;
+        }
+
+        @Override
+        public void apply() {
+            this.manager.addFluid(this.input.getName(), this.output.getName(), heat);
+        }
+
+        @Override
+        public String describe() {
+            return null;
+        }
+    }
+
+}
